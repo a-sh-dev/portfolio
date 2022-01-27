a5ec6201ddf4efc52b91b69c56ddec6a74387294
a-sh <iam.angie@ymail.com>
2022-01-27 17:31:10 +1100

    Initial setup and commit with twin.macro

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import '../../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
