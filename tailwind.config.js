2b343615265922c932ca2059106cffff3b0b209c
a-sh <iam.angie@ymail.com>
2022-01-28 21:51:23 +1100

    Add & configure branding colors + testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+// For custom color variables
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`;
+  };
+}
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -10,6 +20,18 @@ module.exports = {
       fontFamily: {
         sans: ['Work Sans', ...defaultTheme.fontFamily.sans],
         mono: ['Space Mono', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        // transparent: 'transparent',
+        // current: 'currentColor',
+        'primary-light': withOpacityValue('--color-primary-light'),
+        'primary-dark': withOpacityValue('--color-primary-dark'),
+        'accent-teal': withOpacityValue('--color-accent-teal'),
+        'accent-olive': withOpacityValue('--color-accent-olive'),
+        'accent-gray': withOpacityValue('--color-accent-gray'),
+        'accent-pink': withOpacityValue('--color-accent-pink'),
+        'accent-blue': withOpacityValue('--color-accent-blue'),
+        'accent-purple': withOpacityValue('--color-accent-purple'),
       },
     },
   },
