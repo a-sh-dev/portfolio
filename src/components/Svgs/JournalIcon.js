b59c4a721ebbeb99c87b13a2ceeb12bd4427e7da
a-sh <iam.angie@ymail.com>
2022-01-31 15:06:44 +1100

    Create custom Icon components & add images

---
+++ src/components/Svgs/JournalIcon.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const JournalIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 36"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M12.92 0H5.26A5.46 5.46 0 0 0 0 5.55v20.82a2.08 2.08 0 0 0 .29.68l6.79 8a3.09 3.09 0 0 0 2.3 1 3.31 3.31 0 0 0 2.29-1c.1 0 6-8 6-8a2.08 2.08 0 0 0 .29-.68V5.55A5.19 5.19 0 0 0 12.92 0Zm-10 10.12h4.73v14.5H2.87Zm8.58 20.56H7.25l-2.66-3.14h9.28Zm3.81-6.06h-4.78v-14.5h4.78Zm0-17.42H2.87V5.55a2.5 2.5 0 0 1 2.39-2.63h7.66a2.5 2.5 0 0 1 2.39 2.63Z" />
+  </svg>
+);
+
+export default JournalIcon;
