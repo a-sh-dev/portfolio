1c965d6339338343d0452ac5887fe258f838aeec
a-sh <iam.angie@ymail.com>
2022-02-15 01:29:07 +1100

    Clean up codes in ProjectList to reuse InfoBlurb comp

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,25 +1,8 @@
 import tw, { styled } from 'twin.macro';
-import { sectionPaddingB } from '../../../styles/stylesData';
 import { Section } from '../layout';
-import { Em } from '../typography';
 import ProjectCard from './ProjectCard';
-import { FaInfo } from 'react-icons/fa';
-import { Icon, SloganSticker } from '..';
+import { InfoBlurb, SloganSticker } from '..';
 import { projectsData } from '../../data';
-
-const Intro = styled.div(() => [
-  sectionPaddingB,
-  tw`
-    text-primary-dark
-    text-sm
-    flex
-    justify-center
-    gap-4
-    md:items-center
-    lg:text-base
-    xl:text-lg
-  `,
-]);
 
 const SloganWrapper = tw.div`
   hidden
@@ -39,15 +22,10 @@ const ProjectList = () => {
           <SloganSticker withLogo sm lg color="purple" />
         </SloganWrapper>
       </SloganWrapper>
-      <Intro>
-        <Icon size="xs">
-          <FaInfo />
-        </Icon>
-        <Em>
-          Please refer to the project&apos;s repository for detailed
-          information.
-        </Em>
-      </Intro>
+      <InfoBlurb
+        blurb="Please refer to the project's repository for detailed information"
+        sectPad
+      />
       <main className="last:mb-10 mb:last:mb-14 xl:last:mb-20 2xl:last:mb-24">
         {projectsData.map((project, index) => {
           let reverse = index % 2 === 0;
