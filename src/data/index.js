6bdbf3f049bbd5458cb974d4a6764160952c144f
a-sh <iam.angie@ymail.com>
2022-02-16 00:11:19 +1100

    Create initial journalData to fill in

--- src/data/index.js
+++ src/data/index.js
@@ -4,3 +4,4 @@ export * from './skillsData';
 export * from './factsData';
 export * from './projectsData';
 export * from './formData';
+export * from './journalData';
---
+++ src/data/journalData.js
@@ -0,0 +1,22 @@
+export const circa = new Date(2021, 3, 22);
+const dateFormat = 'dd.MM.yy';
+
+const toCopy = {
+  date: '',
+  day: '',
+  tag: '',
+  code: '',
+  note: '',
+  icon: '',
+},
+
+export const journalDays = [
+  {
+    date: '',
+    day: '',
+    tag: '',
+    code: '',
+    note: '',
+    icon: '',
+  },
+];
