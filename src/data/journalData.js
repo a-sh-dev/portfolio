b17d3927c479429ed70b581ea5e22353a7e7f5e5
a-sh <iam.angie@ymail.com>
2022-02-16 00:13:48 +1100

    Install react-easy-emoji & date-fns for Journal section

--- src/data/journalData.js
+++ src/data/journalData.js
@@ -1,14 +1,14 @@
 export const circa = new Date(2021, 3, 22);
 const dateFormat = 'dd.MM.yy';
 
-const toCopy = {
-  date: '',
-  day: '',
-  tag: '',
-  code: '',
-  note: '',
-  icon: '',
-},
+// const toCopy = {
+//   date: '',
+//   day: '',
+//   tag: '',
+//   code: '',
+//   note: '',
+//   icon: '',
+// },
 
 export const journalDays = [
   {
