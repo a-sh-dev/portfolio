8715c7df8a5956f2e1c0b8e13cb5af65258733fd
a-sh-dev <iam.angie@ymail.com>
2024-04-01 18:19:54 +1100

    Animate simple page transition

--- src/components/layout/PageTransition.js
+++ src/components/layout/PageTransition.js
@@ -2,13 +2,17 @@ import { motion } from 'framer-motion';
 import { animate } from '../../utils/animate';
 
 const PageTransition = ({ children }) => {
-  const opacity = {
-    initial: { opacity: 0 },
-    enter: { opacity: 1, transition: { duration: 2 } },
-    exit: { opacity: 1 },
+  const transition = {
+    initial: { opacity: 0, y: 30 },
+    enter: {
+      opacity: 1,
+      y: 0,
+      transition: { type: 'easeIn', duration: 0.7 },
+    },
+    exit: { opacity: 1, y: 30 },
   };
 
-  return <motion.div {...animate(opacity)}>{children}</motion.div>;
+  return <motion.div {...animate(transition)}>{children}</motion.div>;
 };
 
 export default PageTransition;
