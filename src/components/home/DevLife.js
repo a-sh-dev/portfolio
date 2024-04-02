7dc007d46dad9cf3cd5367e88597a928d69eaa02
a-sh-dev <iam.angie@ymail.com>
2024-04-02 21:43:32 +1100

    Update DevLife content

--- src/components/home/DevLife.js
+++ src/components/home/DevLife.js
@@ -24,10 +24,10 @@ const DevLife = () => {
           </Heading>
           <SubHeading align="autoL">The good and the bad</SubHeading>
           <Paragraph>
-            During planning and development for my projects, I tend to spend
+            During project planning and development, I tend to spend
             <em> extra </em>
-            time deciding on the branding for the UI. From choosing the right
-            font, colours, image assets to wireframing (including the
+            time deciding on the branding for the UI, from choosing the right
+            font, colours, and image assets to wireframing (including the
             high-fidelity ones). So, perhaps, that is the downside of the
             hybrid.
           </Paragraph>
@@ -46,7 +46,7 @@ const DevLife = () => {
           <Paragraph>
             I am also driven by curiosity, eager to learn new things. Somehow, I
             find it enjoyable to read other people&apos;s source code to figure
-            out how things work. Especially if it would help me code better and
+            out how things work, especially if it would help me code better and
             improve productivity. Therefore, I welcome other people&apos;s
             constructive feedback and critiques
             <span className="em-dash">---</span>they are precious!
