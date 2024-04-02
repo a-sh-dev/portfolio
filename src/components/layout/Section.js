94fef080d7d5786a85a7971ffe03039095513f1f
a-sh-dev <iam.angie@ymail.com>
2024-04-02 17:15:44 +1100

    Tweak Section padding for small screen

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -4,7 +4,7 @@ import { layoutVariants, sectionVariants } from '../../../styles/stylesData';
 const StyledSection = styled.section(
   ({ relative, noTopMargin, noBottomMargin, halfMargin }) => [
     tw`
-    py-14
+    py-12
     md:py-28
     xl:py-44
     border-primary-dark
