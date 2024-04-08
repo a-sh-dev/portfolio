3d95eea0404c7197ff2ef1e7151103eabab88fb6
a-sh-dev <iam.angie@ymail.com>
2024-04-08 15:45:03 +1000

    Update Designer/Developer content

--- src/components/home/DevLife.js
+++ src/components/home/DevLife.js
@@ -6,7 +6,7 @@ import { HiOutlineSparkles } from 'react-icons/hi';
 
 const DevLife = () => {
   return (
-    <Collapsible color="blue" title="life as a developer">
+    <Collapsible color="blue" title="Developer/Designer Hybrid">
       <GridContainer flowCol>
         <GridColumn col="span-4" start="start-9">
           <div className="text-center my-4 md:my-0 w-full md:grid">
@@ -20,39 +20,57 @@ const DevLife = () => {
         </GridColumn>
         <GridColumn col="span-8" start="start-1">
           <Heading variant="sm" align="autoL">
-            Designer<span className="em-dash-title">---</span>Developer Hybrid
+            Developer<span className="em-dash-title">---</span>Designer Hybrid
           </Heading>
-          <SubHeading align="autoL">The good and the bad</SubHeading>
+          <SubHeading align="autoL">
+            Bridging the gap between design and development
+          </SubHeading>
           <Paragraph>
-            During project planning and development, I tend to spend
-            <em> extra </em>
-            time deciding on the branding for the UI, from choosing the right
-            font, colours, and image assets to wireframing (including the
-            high-fidelity ones). So, perhaps, that is the downside of the
-            hybrid.
+            As a graphic designer turned software developer, I offer a unique
+            cross-disciplinary perspective that seamlessly bridges design and
+            development worlds. My background allows me to approach challenges
+            with a fresh mindset, leading to innovative, user-centric solutions.
           </Paragraph>
           <Paragraph>
-            However, that extra time is purposeful as part of the
-            problem-solving process. In addition, I enjoy translating subject
-            matters by integrating the technical and visual aspects of achieving
-            a well-presented UI and effective UX in every project.
+            I'm a developer who speaks design and a designer who understands
+            development. This unique blend enables me to navigate effortlessly
+            between these disciplines, fostering a more efficient and effective
+            development process.
           </Paragraph>
           <Paragraph>
-            Having experience working in a team and independently, I work better
-            when all my folders and files are well organised and named
-            (including variables!). In addition, I carefully evaluate how I
-            write my codes so that others may easily pick them up afterwards.
+            Grounded in design thinking, I tackle projects through an
+            empathetic, user-centric lens. I excel at translating complex
+            concepts into intuitive and visually appealing user interfaces that
+            are both aesthetically pleasing and highly usable. My keen eye for
+            detail ensures cohesive visual elements and clean, maintainable
+            code.
           </Paragraph>
           <Paragraph>
-            I am also driven by curiosity, eager to learn new things. Somehow, I
-            find it enjoyable to read other people&apos;s source code to figure
-            out how things work, especially if it would help me code better and
-            improve productivity. Therefore, I welcome other people&apos;s
-            constructive feedback and critiques
-            <span className="em-dash">---</span>they are precious!
+            Moreover, my creative mindset fosters out-of-the-box thinking and
+            developing innovative solutions. This creativity and my
+            cross-disciplinary perspective cultivate an environment that
+            welcomes diverse ideas and seamless collaboration.
+          </Paragraph>
+          <Paragraph>
+            As a versatile contributor, I handle all project facets
+            <span className="em-dash">--</span>from visual design to functional
+            development. I champion consistency and maintainability through
+            initiatives like design system enhancements and refactoring.
+            Continuous growth is paramount. I actively seek constructive
+            feedback and critiques, viewing them as precious opportunities for
+            self-improvement
             <TextIcon>
               <HiOutlineSparkles />
             </TextIcon>
+            This openness, combined with my attention to detail and passion for
+            delivering high-quality solutions, enables substantial contributions
+            to any team.
+          </Paragraph>
+          <Paragraph>
+            Ultimately, my unique blend of skills empowers the creation of user
+            experiences that seamlessly blend form and function, elevating
+            aesthetic appeal and usability across a wide range of digital
+            products and services.
           </Paragraph>
         </GridColumn>
       </GridContainer>
